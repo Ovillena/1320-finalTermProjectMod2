94789b2f6e5370ca4d3ae698d10a8c810d625bc2
Octavio Villena <octaviovillena@hotmail.com>
2020-12-04 01:07:08 -0800

    es linted and commit what if have. Stuck at just trying to get images to show on index

--- app.js
+++ app.js
@@ -5,22 +5,27 @@ const path = require('path');
 const fs = require('fs');
 const { readDir, grayScale } = require('./IOhandler');
 const IOhandler = require("./IOhandler");
+const { pipeline } = require('stream');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
     // parse a file upload
-    const form = formidable({ multiples: true, uploadDir: path.join(__dirname, 'uploads'), keepExtensions: true });
+    const form = formidable({
+      multiples: true,
+      uploadDir: path.join(__dirname, 'uploads'),
+      keepExtensions: true,
+    });
 
     form.parse(req, (err, fields, files) => {
-      if(err){
+      if (err) {
         res.writeHead(500, 'Server Error');
         res.end('<h1>error 500</h1>');
       }
       res.writeHead(200, { 'content-type': 'text/html' });
       fs.readFile('./successPage/success.html', (err, data) => {
-        if(err){
+        if (err) {
           res.end('error');
-        }else {
+        } else {
           res.end(data);
         }
       });
@@ -30,32 +35,31 @@ const server = http.createServer((req, res) => {
           pathArray.forEach((element) => {
             grayScale(element, path.join(__dirname, 'grayscale')); // grayscale each image in unzipped folder
           });
-        }) 
-        //use .then to put images into something*****************************************************************************
+        })
         .catch((error) => console.log(error)); // catches errors
     });
     return;
-  } 
-  else if (req.url === '/css/index.css') {
+  } if (req.url === '/css/index.css') {
     res.writeHead(200, { 'context-type': 'text/css' });
     res.write(fs.readFileSync('./css/index.css'));
-  } 
-  else if (req.url === '/photos'){
-    readDir(path.join(__dirname, 'uploads'))
-      .then(() => {
-        res.writeHead(200, { 'context-type': 'image/png'})
-        
-      })
-      .catch(error => console.log(error));
+  } else if (req.url === '/grayscale/upload_b3d96eecccf9663c3a3f2317bbbb3149.png') {
+    res.writeHead(200, { 'content-type': 'image/png' });
+    const pngImage = fs.readFileSync(path.join(__dirname, 'uploads', 'upload_b3d96eecccf9663c3a3f2317bbbb3149.png'));
+    pngImage.pipe(res);
+    // readDir(path.join(__dirname, 'uploads'))
+    //   .then((elements) => {
+    //     let pngImage = fs.createReadStream(elements[0]);
+    //     pngImage.on('error', () => console.log('error').on('finish', (data) => res.write(data)));
+    //   })
+    //   .catch(error => console.log(error));
   }
 
-  else if(req.url === '/'){
+  else if (req.url === '/') {
     // show a file upload form
     res.writeHead(200, { 'content-type': 'text/html' });
     res.write(fs.readFileSync('./index.html'));
   }
   res.end();
-  
 });
 
 server.listen(8000, () => {
